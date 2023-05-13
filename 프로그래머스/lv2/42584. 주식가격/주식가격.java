c2a2c4694d2d4e599463c722ea67d9165e577ef9
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-05-13 11:25:45 +0900

    [level 2] Title: 주식가격, Time: 12.80 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] prices) {
+		int [] answer = new int [prices.length];
+		
+		for(int i=0;i<prices.length;i++) {
+			for(int j=i;j<prices.length-1;j++) {
+				if(prices[i]<=prices[j]){answer[i]++;}
+                else {break;}
+			}
+		}
+        return answer;
+    }
+}
