598cb09198169aa51ff7a86d7dde6538d3590340
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-05-13 11:16:28 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+      int [] answer = new int [commands.length];
+      for(int a=0;a<commands.length;a++){
+        int i = commands[a][0];
+        int j = commands[a][1];
+        int k = commands[a][2];
+
+        int [] arr = new int[j-i+1];
+        for(int b=0;b<arr.length;b++){
+          arr[b]=  array[i+b-1];  
+        }
+        Arrays.sort(arr);
+        answer[a]=arr[k-1];
+      }
+        return answer;
+    }
+}
