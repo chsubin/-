5a86533698523d520ee243c2f2d46145e60bff1f
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-05-13 11:27:38 +0900

    [level 2] Title: 가장 큰 수, Time: 16.48 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+		String answer ="";
+		
+		PriorityQueue<Integer> queue = new PriorityQueue<>((o1,o2)->{
+			String str1 = o1+"";
+			String str2 = o2+"";
+			int result;
+			if( str1.charAt(0)!=str2.charAt(0)) {
+				result = str1.charAt(0)-str2.charAt(0);
+			}
+			else {
+				result = (int)(Long.parseLong(str1+str2)-Long.parseLong(str2+str1));
+			}
+			return -result;
+		});
+		for(int a:numbers) {
+			queue.add(a);
+		}
+		boolean flag = false;
+		while(!queue.isEmpty()) {
+			int a = queue.poll();
+			if(a!= 0) {
+				flag = true;
+			}
+			answer +=a;
+		}
+		if(!flag) {
+			answer ="0";
+		}
+        return answer;
+    }
+}
