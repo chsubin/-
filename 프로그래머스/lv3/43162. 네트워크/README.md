bdf4df6fb1b27877cdea26ad2bd52083d0be0af7
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-05-26 17:07:30 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/README.md
+++ 프로그래머스/lv3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 1.47 ms
+메모리: 75.7 MB, 시간: 0.47 ms
 
 ### 구분
 
--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,45 +1,37 @@
 import java.util.*;
 class Solution {
-    static ArrayList<Integer> [] A;
-    static boolean visit[] ;
+    static ArrayList<Integer> A[];
+    static boolean visit[];
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        A =  new ArrayList[computers.length];
-        visit = new boolean[A.length];
-        
+        A = new ArrayList[n];
+        visit = new boolean[n];
         for(int i=0;i<computers.length;i++){
-            A[i] = new ArrayList<Integer>();
+            A[i] = new ArrayList<>();
             for(int j=0;j<computers.length;j++){
-                int S = i;
-                int E = j;
-                if(S!=E&&computers[S][E]==1){
-                A[S].add(E);
+                if(computers[i][j] ==1){
+                    A[i].add(j);
                 }
             }
         }
-        for(int i=0;i<computers.length;i++){
+        for(int i=0;i<n;i++){
             if(!visit[i]){
-                DFS(i);
                 answer++;
+                DFS(i);
             }
             
         }
         
         
+        
         return answer;
     }
-    private static void DFS(int now){
-        if(visit[now]) return;
-        System.out.println(now);
-        visit[now] = true;
-        for(int i:A[now]){
-            if(!visit[i]){
-                DFS(i);
-            }
+    public static void DFS(int i){
+        if(visit[i]) return;
+        visit[i] = true;
+        for(int v:A[i]){
+            DFS(v);
         }
-        
-        
-        
         
     }
 }
