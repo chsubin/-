35320b512f8a1b25fe6491d6d4d7c3b4e11892f5
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-05-26 17:25:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.54 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,40 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.*;
 class Solution {
-	static int m;
-	static int n;
-	static boolean visited[][];
-	static int dx [] = {0,-1,0,1};
-	static int dy [] = {-1,0,1,0};
-	static int [][] map;
-	static int  answer =0;
+    static int [][] map ;
+    static boolean [][] visit;
+    static int answer = 0;
     public int solution(int[][] maps) {
-		map = maps;
-		m = maps.length;
-		n = maps[0].length;
-		visited = new boolean[m][n];
-		
-		BFS(new Node(0, 0, 1));
-		
+        map = maps;
+        visit = new boolean [map.length][map[0].length];
+        
+        BFS(maps.length-1,maps[0].length-1);
+        
         return answer==0?-1:answer;
     }
-	private static void BFS(Node node) {
-		Queue<Node> queue = new LinkedList<>();
-		queue.add(node);
-		visited[node.row][node.col]= true;
-		while(!queue.isEmpty()) {
-			Node now = queue.poll();
-			if(now.row==m-1&&now.col==n-1) {
-				answer = now.depth;
-				return;
-			}
-			for(int i=0;i<4;i++) {//상하좌우탐색
-				int next_row = now.row+dy[i];
-				int next_col = now.col+dx[i];
-				if(next_row>=m||next_row<0)continue;
-				else if(next_col>=n||next_col<0)continue;
-				if(!visited[next_row][next_col]&&map[next_row][next_col]==1) {
-					queue.add(new Node(next_row, next_col,now.depth+1));
-					visited[next_row][next_col]= true;
-				}
-			}
-		}
-	}
-}
-class Node{
-	int row;
-	int col;
-	int depth;
-	public Node(int row, int col, int depth) {
-		this.row = row;
-		this.col = col;
-		this.depth = depth;
-	}
+    public static void BFS(int n,int m){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int []{0,0,1});
+        while(!queue.isEmpty()){
+            int [] A = queue.poll();
+            int x = A[0];
+            int y = A[1];
+            int depth = A[2];
+            if(visit[x][y]) continue;
+            if(x==n&&y==m) {answer = depth;return;}
+            visit[x][y] = true;
+            int [] dx = {-1,0,1,0};
+            int [] dy = {0,-1,0,1};
+            for(int i=0;i<4;i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if(nx>n||nx<0||ny>m||ny<0) continue;
+                if(map[nx][ny]==0) continue;
+                queue.add(new int[]{nx,ny,depth+1});
+            }
+            
+            
+        }
+        
+        
+    }
 }
