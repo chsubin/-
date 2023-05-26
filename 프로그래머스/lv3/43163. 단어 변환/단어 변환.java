98f438b66ba079717f25b15a44e02cddee05f1c5
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-05-26 18:21:03 +0900

    [level 3] Title: 단어 변환, Time: 0.08 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -1,54 +1,38 @@
 import java.util.*;
 class Solution {
     static boolean visit[];
-    static String word[];
-    static int answer=0;
+    static int answer = 101;
     public int solution(String begin, String target, String[] words) {
-        word = words;
         visit = new boolean[words.length];
+        BFS(begin,target,words);
         
-        BFS(begin,target);
-        
-        return answer;
+        return answer==101?0:answer;
     }
-    private static void BFS(String begin,String target){
-        Queue<Node> queue = new LinkedList<>();
-        
-        for(int i=0;i<word.length;i++){
-            int su =0;
-            for(int j=0;j<begin.length();j++){
-                if(!word[i].substring(j,j+1).equals(begin.substring(j,j+1))) su++;
-            }
-            if(su ==1) {queue.add(new Node(i,1));}
+    public static void BFS(String begin, String target,String[]words){
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i=0;i<words.length;i++){
+            if(go(words[i],begin)) queue.add(new int []{i,1});
         }
         while(!queue.isEmpty()){
-            Node node = queue.poll();
-            String str = word[node.index];
-            int depth = node.depth;
-            if(visit[node.index]) continue;
-            visit[node.index] = true;
-            if (str.equals(target)) {answer = depth;System.out.println(str);return;}
-            
-            for(int i=0;i<word.length;i++){
-                if(!visit[i]){
-                    int su =0;
-                    for(int j=0;j<str.length();j++){
-                        if(!word[i].substring(j,j+1).equals(str.substring(j,j+1))) su++;
-                    }
-                    if(su ==1) {queue.add(new Node(i,depth+1));}
-                }
-            }
-            
+            int [] A = queue.poll();
+            int index = A[0];
+            int depth = A[1];
+            if(visit[index]) continue;
+            visit[index] = true;
+            if(words[index].equals(target)) {answer = depth; return;}
+            for(int i=0;i<words.length;i++){
+                if(go(words[index],words[i])) queue.add(new int[]{i,depth+1});
+            }  
         }
-        
     }
-}
-class Node{
-    int index;
-    int depth;
-    Node(int index, int depth){
-        this.index = index;
-        this.depth = depth;
+    public static boolean go(String str1, String str2){
+        int su = 0;
+        for(int i=0;i<str1.length();i++){
+            if(str1.charAt(i)!=str2.charAt(i)){
+                su++;
+            }
+        }
+        if(su==1) return true;
+        else return false;  
     }
-    
 }
