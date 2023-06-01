02ef26ba65a327308fa68ec2aac6e0b8d40acef5
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-01 21:45:55 +0900

    [unrated] Title: 디펜스 게임, Time: 1.30 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -2,24 +2,25 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>((o1,o2)->{
-            return o2-o1; //내림차순으로 우선순위 큐 정렬
-        });
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1,o2)->{return o2-o1;});
         int sum = 0;
+        answer = enemy.length;
         for(int i=0;i<enemy.length;i++){
-            queue.add(enemy[i]);
             sum += enemy[i];
-            if(sum>n){
-                while(k>0){
-                    int a = queue.poll();
-                    sum -= a;
+            queue.add(enemy[i]);
+            while(sum>n){
+                if(k==0) {return i;}
+                else{
                     k--;
-                    if(sum<=n) break;
+                    int a = queue.poll();
+                    sum-= a;
                 }
-                if(sum>n) return i;
+                
             }
-        };
+            
+        }
         
-        return enemy.length;
+        
+        return answer;
     }
 }
