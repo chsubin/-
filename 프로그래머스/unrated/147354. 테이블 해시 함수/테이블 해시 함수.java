4362121c00056b6c5999956a6844b94120481a54
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-02 18:51:56 +0900

    [unrated] Title: 테이블 해시 함수, Time: 1.04 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -1,26 +1,24 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
-        int answer = 0;
-        Arrays.sort(data,(int []o1,int[] o2)->{
-            if (o1[col-1]!=o2[col-1]){
-                return o1[col-1]-o2[col-1]; //col번재 컬럼 오름차순
+        Arrays.sort(data,(o1,o2)->{ //정렬
+            if(o1[col-1]!=o2[col-1]){
+                return o1[col-1]-o2[col-1];
             }
-            else {
-                return o2[0]-o1[0]; //첫번쨰 컬럼 내림차순
+            else return  o2[0]-o1[0];
+        });
+        int answer= 1;
+        for(int i=row_begin-1;i<=row_end-1;i++){
+            int sum = 0;
+            for(int j=0;j<data[0].length;j++){
+                sum+=data[i][j]%(i+1);
             }
-        }); //정렬
-        int res = 0;
-        for(int i=row_begin-1;i<row_end;i++){
-            int a = 0;
-            for(int j=0;j<data[i].length;j++){
-                a+=data[i][j]%(i+1);
-            }
-            if(i==row_begin-1) res = a;
-            else res = res^a;
+            if(i==row_begin-1) answer = sum;
+            else answer = answer^sum;
         }
         
-        answer = res;
+        
+        
         return answer;
     }
 }
