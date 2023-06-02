f10b973b02d65d6f02e8e9b1e3085ee39ddbc8f9
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-02 21:18:49 +0900

    [level 2] Title: 후보키, Time: 24.47 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.java
+++ 프로그래머스/lv2/42890. 후보키/후보키.java
@@ -1,65 +1,63 @@
 import java.util.*;
 class Solution {
-    static ArrayList<String> A;
-    static String[][] relations;
+    static ArrayList<String> A = new ArrayList<>();
     public int solution(String[][] relation) {
-        int answer =0;  
-        A = new ArrayList<>();
-        relations = relation;
-        for(int i=0;i<relations[0].length;i++){
-            DFS(i,""+i);
+        int answer = 0;
+        DFS(0,relation[0].length,"");
+        Collections.sort(A,(o1,o2)->{
+            return o1.length() - o2.length();
+        });
+        
+        for(int i=0;i<A.size();i++){
+            if(!frustrate(A.get(i),relation)){A.remove(i);i--;}
         }
-    Collections.sort(A,(o1,o2)->{
-        return o1.length()-o2.length();
-    });
-    while(A.size()!=0){
-       String sample [] = new String [relations.length];
-        for(int i=0;i<sample.length;i++){
-            sample[i] = "";
-        }
-        String strs = A.get(0);
-        for(int i=0;i<strs.length();i++){
-            int sub = Integer.parseInt(strs.substring(i,i+1));
-            for(int j=0;j<relations.length;j++){
-                sample[j] += relations[j][sub];
-            }
-        }
-        int sw = 0;
-        for(int i=0;i<sample.length;i++){
-            for(int j=i+1;j<sample.length;j++){
-                if(sample[i].equals(sample[j])){
-                    sw=1;
+        for(int i=0;i<A.size();i++){
+            for(int j=i+1;j<A.size();j++){
+                if(include(A.get(i),A.get(j))){
+                    A.remove(j); j--;
                 }
             }
         }
-        if(sw==1){
-            A.remove(0);
-        }
-        else{
-            System.out.println(strs);
-            answer++;
-            A.remove(0);
-              for(int j=0;j<A.size();j++){
-                int sum =0;
-                    for(int i=0;i<strs.length();i++){
-                        String sub = strs.substring(i,i+1);
-                    if(A.get(j).contains(sub)&&A.get(j).length()>strs.length()) {
-                            sum++;
-                    }
-                }
-                if(sum==strs.length()) {
-                    A.remove(j);
-                    j--;
-                }
-            }
-        }
-    }return answer;
+        System.out.println();
+        
+        
+        
+        return A.size();
     }
-      private static void DFS(int now,String str){
-        if(A.contains(str))return;
-        A.add(str);
-        for(int i = now+1 ;i <relations[0].length;i++){
-            DFS(i,str+i);
+    private static boolean frustrate(String str,String [][]relation){
+        String [] imsi = new String [relation.length];
+        for(int i=0;i<imsi.length;i++){
+            imsi[i] = "";
         }
+        for(int i=0;i<str.length();i++){
+            int index = Integer.parseInt(str.substring(i,i+1));
+            for(int j=0;j<imsi.length;j++){
+                imsi[j] += relation[j][index]+"/";
+            }
+        }
+        Arrays.sort(imsi);
+        for(int i=1;i<imsi.length;i++){
+            if(imsi[i].equals(imsi[i-1])) return false;
+        }
+        return true;
+        
+        
     }
+    private static boolean include(String str1, String str2){
+        //str2가 str1의 모든 원소를 포함하는지 확인하는 함수
+        boolean result = true;
+        for(int i=0;i<str1.length();i++){
+            if(!str2.contains(str1.substring(i,i+1))) result = false;
+        }
+        return result;
+    }
+    private static void DFS(int idx,int limit,String str){
+        if(!A.contains(str)&&!str.equals("")){A.add(str);}
+        if(idx==limit) return;
+        DFS(idx+1,limit,str+idx);
+        DFS(idx+1,limit,str);
+        
+        
+    }
+    
 }
