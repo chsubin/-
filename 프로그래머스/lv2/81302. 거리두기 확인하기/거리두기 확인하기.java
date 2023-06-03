17cab9245c958bfe92dcfb194c1b7c790004c01d
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-03 18:47:49 +0900

    [level 2] Title: 거리두기 확인하기, Time: 8.77 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,53 +1,75 @@
+import java.util.*;
 class Solution {
-    static int result =1;
     public int[] solution(String[][] places) {
         int[] answer = new int [places.length];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = 1;
+        }
         for(int i=0;i<places.length;i++){
-            result =1;
-            char [][] seat = new char[5][5];
+            String [][] A = new String[5][5];
+            System.out.println("i"+i);
             for(int j=0;j<5;j++){
                 for(int k=0;k<5;k++){
-                    seat[j][k] = places[i][j].charAt(k);
+                    A[j][k] = places[i][j].substring(k,k+1);
                 }
             }
+            boolean result = true;
             for(int j=0;j<5;j++){
                 for(int k=0;k<5;k++){
-                    seat[j][k] = places[i][j].charAt(k);
-                }
-            }
-            for(int j=0;j<5;j++){
-                for(int k=0;k<5;k++){
-                    if(seat[j][k]=='P'){
-                    DFS(j,k,j,k,"",seat);
+                    if(A[j][k].equals("P")){
+                        if(!BFS(j,k,A)) answer[i] = 0;
                     }
                 }
             }
-    
-            answer[i] = result;
+            
         }
         return answer;
     }
-    private static void DFS(int orr,int orc,int row,int col,String str,char [][] seat){
-        int drow[] ={-1,0,1,0};
-        int dcol[] ={0,-1,0,1};
-        if(str.equals("P")){
-            result =0;
-            return;
+    private static boolean BFS(int rowss, int colss ,String [][]places){
+        boolean result = true;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(rowss,colss,false,0));
+        while(!queue.isEmpty()){
+            Node node = queue.poll(); //현재 탐색할 노드
+            int [] dx = {-1,0,1,0};
+            int [] dy = {0,-1,0,1};
+            int row = node.row;
+            int col = node.col;
+            boolean part = node.part;
+            int depth= node.depth;
+            if(depth>=3) continue;;
+            if(places[row][col].equals("X")) part = true;
+            if(depth==1&&places[row][col].equals("P")) {
+                return false;}
+            else if(depth==2&&!(row==rowss&&col==colss)){
+                if(places[row][col].equals("P")&&!part){
+                    return false;
+                }
+            }
+            for(int i=0;i<4;i++){
+                int nrow = row+dx[i];
+                int ncol = col+dy[i];
+                if(nrow>=places.length||nrow<0) continue;
+                if(ncol>=places[0].length||ncol<0) continue;
+                queue.add(new Node(nrow,ncol,part,depth+1));
+                queue.add(new Node(nrow,ncol,part,depth+1));
+            }
+            
         }
-        else if(str.equals("OP")){
-            result= 0;
-            return;
-        }
-        if(str.length()==2) return;
-        for(int i=0;i<4;i++){
-            int newrow = row+drow[i];
-            int newcol = col+dcol[i];
-            if(newrow==orr&&newcol==orc) continue;
-            if(newrow>=5||newrow<0||newcol>=5||newcol<0) continue;
-            DFS(orr,orc,newrow,newcol,str+seat[newrow][newcol],seat);
-        }
-        
-        
+        return result;
         
     }
+}
+class Node{
+    int row;
+    int col;
+    boolean part;
+    int depth;
+    Node(int row, int col, boolean part, int depth){
+        this.row = row;
+        this.col = col;
+        this.part = part;
+        this.depth= depth;
+    }
+    
 }
