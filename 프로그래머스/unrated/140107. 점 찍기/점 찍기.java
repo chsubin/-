75ef83a11a9067a19e9124d48104fcfa327560fa
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-03 21:20:44 +0900

    [unrated] Title: 점 찍기, Time: 0.09 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
@@ -1,21 +1,12 @@
-import java.util.*;
-class Solution {
-    static ArrayList<Integer> list;
+class Solution {//상수, 거리
     public long solution(int k, int d) {
         long answer = 0;
-        list = new ArrayList<>();
-        int su = 0;
-        while(true){
-            if(su*k>d) break;
-            list.add(su*k);
-            su++;
+        for(long i=0;k*i<=d;i++){
+            long x = i*k;
+            long y = (long)Math.sqrt((long)d*d-(long)x*x);
+            answer += y/k+1;
         }
-        for(int i=0;i<list.size();i++){
-            int a = list.get(i);
-            int b = (int)Math.sqrt((long)d*d-(long)a*a);
-            answer++;
-            answer+=b/k;
-        }
+        
         
         return answer;
     }
