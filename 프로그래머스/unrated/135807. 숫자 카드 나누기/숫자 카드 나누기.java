e1e8ba42ff48903e0e1f3f6292e646e3f5731d23
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-03 17:08:04 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,20 +1,15 @@
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
-        int answer = 0;
         int agcd = arrayA[0];
         int bgcd = arrayB[0];
-        if(arrayB.length>1){
-            agcd = gcd(arrayA[0],arrayA[1]);
-            bgcd = gcd(arrayB[0],arrayB[1]);
+        if(arrayA.length>1){
+            for(int i=1;i<arrayA.length;i++){ //최대공약수 구하기
+                agcd = gcd(arrayA[i],agcd);
+                bgcd = gcd(arrayB[i],bgcd);
+            }
         }
-        for(int i=2;i<arrayA.length;i++){
-            agcd = gcd(arrayA[i],agcd);
-        }
-        for(int i=2;i<arrayB.length;i++){
-            bgcd = gcd(arrayB[i],bgcd);
-        }
-        int su1= agcd;
-        int su2=bgcd;
+        int su1 = agcd;
+        int su2 = bgcd;
         for(int i=0;i<arrayA.length;i++){
             if(arrayB[i]%agcd==0) {
                 su1 =0;
@@ -23,19 +18,14 @@ class Solution {
                 su2 = 0;
             }
         }
-        answer = su1>=su2?su1:su2;
-        return answer;
+        return su1>su2?su1:su2;
     }
-    private static int gcd(int a , int b){
+    private static int gcd(int a,int b){
         if(a<b){
-            int temp = a; a =b ;b=temp;
+            int temp = a; a = b; b = temp;
         }
-        if(a%b==0){
-            return b;
-        }
-        else{
-            return gcd(b,a%b);
-        }
+        if(a%b==0) return b;
+        else return gcd(b,a%b);
         
     }
 }
