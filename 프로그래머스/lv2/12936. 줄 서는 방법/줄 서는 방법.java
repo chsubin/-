debf6a094617fa2ab1a5b9f7aecc4b6c166ac418
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-03 23:13:01 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -1,22 +1,22 @@
 import java.util.*;
 class Solution {
-    static ArrayList<Integer> A ;
+    static ArrayList<Integer>A;
     public int[] solution(int n, long k) {
-          A  = new ArrayList<>();
+        A = new ArrayList<>();
         for(int i=0;i<n;i++){
             A.add(i+1);
         }
+        int [] answer = DFS(n,k-1,new int[n],0);
         
-       int answer [] = DFS(n,k-1,new int [n],0);
         return answer;
     }
-      private static int []  DFS(int su, long k , int [] arr , int start){
+    private static int [] DFS(int su, long k, int [] arr, int start){
         if(A.size()==0){
             return arr;
         }
-        //팩토리얼구하기
+        //팩토리얼 구하기
         long f = 1;
-        for(int i = su-1;i>=1;i--){
+        for(int i= su-1;i>=1;i--){
             f = f*i;
         }
         int papa = (int)(k/f);
@@ -24,7 +24,5 @@ class Solution {
         arr[start] = A.get(papa);
         A.remove(papa);
         return DFS(su-1,child,arr,start+1);
-
-
     }
 }
