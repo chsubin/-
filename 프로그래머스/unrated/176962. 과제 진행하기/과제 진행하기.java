0bcb0b49390654cca40755706fdf0e81caed913b
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-03 20:39:06 +0900

    [unrated] Title: 과제 진행하기, Time: 21.31 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.java
@@ -2,53 +2,53 @@ import java.util.*;
 class Solution {
     public String[] solution(String[][] plans) {
         String[] answer = new String [plans.length];
-        Arrays.sort(plans,(String [] o1,String[]o2)->{
-           return o1[1].compareTo(o2[1]);
+        Arrays.sort(plans,(o1,o2)->{ //정렬
+            int time1 = Integer.parseInt(o1[1].substring(0,2))*60+
+               Integer.parseInt(o1[1].substring(3));
+            int time2 = Integer.parseInt(o2[1].substring(0,2))*60+
+               Integer.parseInt(o2[1].substring(3));
+            return time1 -time2;
         });
-        Stack<String []> stack = new Stack<>();
-        int cur_time =0;
-        String cur_sub ="";
+        ArrayList<String> list = new ArrayList<>();
+        int cur_time = 0;
         int end_time = 0;
-        int index =0;
+        String cur_sub = "";
+        Stack<String []> stack = new Stack<>();
         for(int i=0;i<plans.length;i++){
-            cur_time =Integer.parseInt(plans[i][1].substring(0,2))*60 + Integer.parseInt(plans[i][1].substring(3));
-            if(cur_time<end_time){
-                stack.add(new String[]{cur_sub,end_time-cur_time+""});
-            }//현재과목 저장하기
-            else{//멈춘과목 꺼내오기
-                if(!cur_sub.equals("")){
-                    answer[index] = cur_sub;
-                    index++;
+            cur_time = Integer.parseInt(plans[i][1].substring(0,2))*60+
+               Integer.parseInt(plans[i][1].substring(3));
+            if(i!=0){
+                if(cur_time<end_time){//시간이 부족한경우
+                    stack.add(new String[]{cur_sub,end_time-cur_time+""});
                 }
-                int time = cur_time-end_time;
-                while(!stack.isEmpty()){
-                    String [] subs = stack.pop();
-                    if(time>=Integer.parseInt(subs[1])){
-                       time -= Integer.parseInt(subs[1]);
-                        answer[index] = subs[0];
-                        index++;
+                else{ //시간이 딱 맞거나 남는경우
+                    list.add(cur_sub);
+                    int time = cur_time-end_time; //남은시간
+                    while(time>0&&!stack.isEmpty()){ //시간이 남는경우
+                        String A [] = stack.pop();
+                        cur_sub = A[0];
+                        int last_time = Integer.parseInt(A[1]);
+                        if(last_time<=time){
+                            list.add(cur_sub);
+                            time-=last_time;
+                        }
+                        else{
+                            stack.add(new String[]{cur_sub,last_time-time+""});
+                            time = 0;
+                        }
                     }
-                    else{
-                        subs[1] = Integer.parseInt(subs[1])-time+"";
-                        stack.add(subs);
-                        time=0;
-                    }
-                    if(time==0)break;
                 }
             }
-            cur_sub = plans[i][0];//현재과목
-            end_time = cur_time+Integer.parseInt(plans[i][2]);
-
+            cur_sub = plans[i][0];
+            end_time = cur_time +Integer.parseInt(plans[i][2]);   
         }
-        answer[index] = cur_sub;
-        index++;
+        list.add(cur_sub);
         while(!stack.isEmpty()){
-            String [] subs = stack.pop();
-            answer[index] = subs[0];
-            index++;
+            list.add(stack.pop()[0]);
         }
-        
-        
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
         
         
         return answer;
