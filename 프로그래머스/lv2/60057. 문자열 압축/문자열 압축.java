bba12ddeaa02def9dea81a037c4d8d67b9538686
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-03 16:25:41 +0900

    [level 2] Title: 문자열 압축, Time: 7.18 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -1,27 +1,28 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
         int min = 2000;
-        for(int i=1;i<s.length();i++){ //문자열단위의 개수
+        for(int i=1;i<s.length();i++){
+            //문자열 압축단위
             int len = s.length();
             for(int j=i;j<=s.length();j+=i){
-                int replay =0;
+                int replay = 0; //j가 기준
                 while(j+i<=s.length()){
-                    String a = s.substring(j-i,j);
-                    String b = s.substring(j,j+i);
-                    if(a.equals(b)) {
-                        replay+=i;
-                       j+=i;
+                    String a = s.substring(j-i,j);//전
+                    String b = s.substring(j,j+i);//후
+                    if(a.equals(b)){ //전후비교하여 같으면 반복된거임
+                        replay++;
+                        j+= i;
                     }
                     else break;
                 }
                 if(replay!=0){
-                    String str = (replay/i+1)+"";
-                    len = len-replay+str.length();
+                    String str = replay +1 +"";
+                    len = len -replay*i +str.length();
                 }
-            } 
-            if(min>len) {min = len;}
+            }
+            if(min>len){min = len;}
         }
         return min==2000?1:min;
+        //return min;
     }
 }
