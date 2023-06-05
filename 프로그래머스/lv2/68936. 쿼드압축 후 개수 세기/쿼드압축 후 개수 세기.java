c2c19af139f017d6bdca9117ffda4223adf46c17
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-05 23:53:41 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 13.92 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,41 +1,39 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 class Solution {
     public int[] solution(int[][] arr) {
-		int n = arr.length;
-		
-		Queue<int[]> queue = new LinkedList<>();
-		
-		queue.add(new int [] {0,arr.length-1,0,arr.length-1});
-		int one=0;
-		int zero = 0;
-		while(!queue.isEmpty()) {
-			int A [] = queue.poll();
-			int tile = arr[A[0]][A[2]];
-			boolean flag = true;
-			if(A[0]!=A[1]) {
-				for(int i=A[0];i<=A[1];i++) {
-					for(int j=A[2];j<=A[3];j++) {
-						if(tile!=arr[i][j]) {
-							flag = false;
-							break;
-						}
-					}
-				}
-			}
-			if(!flag) {
-				queue.add(new int[] {A[0],(A[0]+A[1])/2,A[2],(A[2]+A[3])/2});
-				queue.add(new int[] {(A[0]+A[1])/2+1,A[1],A[2],(A[2]+A[3])/2});
-				queue.add(new int[] {A[0],(A[0]+A[1])/2,(A[2]+A[3])/2+1,A[3]});
-				queue.add(new int[] {(A[0]+A[1])/2+1,A[1],(A[2]+A[3])/2+1,A[3]});
-			}
-			else {
-				if (tile==1) {one++;}
-				else {zero ++;}
-				
-			}
-		}
-		int [] answer = {zero,one};
+        int [] answer = new int [2];
+        int n = arr.length-1;
+        
+        Queue<int []>queue = new LinkedList<>();
+        queue.add(new int[]{0,n,0,n});
+        while(!queue.isEmpty()){
+            int A [] = queue.poll();
+            int row1 = A[0];int row2 = A[1];int col1 = A[2];int col2 = A[3];
+            //다 같은지 봐야함
+            int su = arr[row1][col1];
+            boolean flag = true;
+            for(int i = row1;i<=row2;i++){
+                for(int j=col1;j<=col2;j++){
+                    if(su!=arr[i][j])flag = false;
+                }
+            }
+            if(flag){ //원소가 다 같을떄
+                answer[su]++;
+            }
+            else {//다를떄
+                int row3= (row1+row2)/2;
+                int col3= (col1+col2)/2;
+                queue.add(new int []{row1,row3,col1,col3});
+                queue.add(new int []{row1,row3,col3+1,col2});
+                queue.add(new int []{row3+1,row2,col1,col3});
+                queue.add(new int []{row3+1,row2,col3+1,col2});
+            }
+            
+            
+        }
+        
+        
+        
         return answer;
     }
 }
