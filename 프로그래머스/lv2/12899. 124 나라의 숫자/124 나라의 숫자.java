69974931f566ca3c041d4fbe58824460ac8cffd1
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-05 20:04:06 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,23 +1,27 @@
+import java.util.*;
 class Solution {
-	static int A [] = {1,2,4};
     public String solution(int n) {
-        return 	FIND(n).toString();
+        String answer = find(n-1).toString();
+        
+        return answer;
     }
-	private static StringBuilder FIND(int n) {
-		int a = (n-1)/3-1;
-		int b = (n-1)%3;
-		StringBuilder sb = new StringBuilder(); 
-		if(a<0) {
-			sb.append(A[b]);
-		}
-		else if (a<3){
-			sb.append(A[a]);
-			sb.append(A[b]);
-		}
-		else {
-			sb.append(FIND(a+1));
-			sb.append(A[b]);
-		}
-		return  sb;
-	}
+    private static  StringBuilder find(int n){
+        String [] A = {"1","2","4"};
+        StringBuilder sb = new StringBuilder();
+        int a = n/3-1;
+        int b = n%3;
+        if(a<0){
+            sb.append(A[b]);
+        }
+        else if(a<3){
+            sb.append(A[a]);
+            sb.append(A[b]);
+        }
+        else{
+            sb.append(find(a));
+            sb.append(A[b]);
+        }
+        return sb;
+        
+    }
 }
