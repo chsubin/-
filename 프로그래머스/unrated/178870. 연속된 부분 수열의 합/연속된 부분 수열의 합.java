19b40e1a1948e3c5a906ef088d2688b076dddec1
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-05 11:35:08 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -2,50 +2,32 @@ class Solution {
     public int[] solution(int[] sequence, int k) {
         int[] answer = new int [2];
         int start = 0;
-        int last = 0;
+        int len =1000001; 
+        int end = 0;
         int sum = sequence[0];
-        int min =-1;
-        while(last<sequence.length){
-            if(last<start) break;
-            if(sum<k){
-                last++;
-                if(last==sequence.length) break;
-                sum +=sequence[last];
-            }
-            else if(sum >k){
+        while(true){
+            if(sum==k){
+                if(len>end-start){
+                    answer[0] = start;
+                    answer[1] = end;
+                    len = end-start;
+               }
+                sum -= sequence[start];
                 start++;
-                sum -= sequence[start-1];
             }
-        else if(sum==k){
-            int len = last - start+1;
-
-            if (min == -1) {
-                min = len;
-                answer[0] = start;
-                answer[1] = last;
-            }
-            else if(min>len){
-                min = len;
-                answer[0] = start;
-                answer[1] = last;
-            }
-            if(last+1<=sequence.length-1){
-                last++;
-                sum +=sequence[last];
-                continue;
-            }
-            else if(start+1<=sequence.length-1){
+            else if(sum>k){
+                sum -= sequence[start];
                 start++;
-                sum -= sequence[start-1];
-                continue;
             }
-            break;
+            else {
+                end++;
+                if(end == sequence.length) break;
+                sum+= sequence[end];
+                
+            }
+            
+            
         }
-
-        }
-        
-        
-        
         return answer;
     }
 }
