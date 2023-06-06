38a12df4d3b4a4bfdbdde1865095b6c9004144d6
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-06 17:52:42 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 70.05 ms, Memory: 207 MB -BaekjoonHub

--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,33 +1,32 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-		int [] answer = new int [numbers.length];
-		answer[numbers.length-1] = -1;
-		
-		int max = numbers[numbers.length-1];
-		for(int i=numbers.length-2;i>=0;i--) {
-			if(max<numbers[i]) {
-				max = numbers[i];
-			}
-			if(numbers[i]<numbers[i+1]) {
-				answer[i] = numbers[i+1];
-			}
-			else {
-					if(numbers[i]>=max) {
-						answer[i] = -1;
-					}
-					else {
-						int sw =0;
-						for(int j=i+1;j<answer.length;j++) {
-							if(numbers[i]<answer[j]) {
-								answer[i] = answer[j];
-								sw =1;
-								break;
-							}
-						}
-						if(sw ==0) answer[i] = -1;
-					}
-			}
-		}
+        int[] answer = new int[numbers.length];
+        Stack<Node> stack = new Stack<>();
+        for(int i=0;i<numbers.length;i++){
+            int number = numbers[i];
+            while(!stack.isEmpty()){
+                Node node = stack.peek();
+                if(node.su<number){
+                    answer[node.index] = number;
+                    stack.pop();
+                }
+                else break;
+            }
+            stack.add(new Node(i,number));
+        }
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+            answer[node.index] = -1;
+        }
         return answer;
+    }
+}
+class Node{
+    int index;
+    int su;
+    Node(int index, int su){
+        this.index = index;
+        this.su = su;
     }
 }
