b371ffec4ccd68dd1a8bb6ee438df50899f90796
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-06 14:38:50 +0900

    [unrated] Title: 롤케이크 자르기, Time: 201.36 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,23 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
-		TreeMap<Integer, Integer> map1 = new TreeMap<>();
-		TreeMap<Integer, Integer> map2 = new TreeMap<>();
-		
-		for(int i=0;i<topping.length;i++) {
-			map1.put(topping[i], map1.getOrDefault(topping[i],0)+1);
-		}
-		int answer =0;
-		int now_index=0;
-		while(now_index<topping.length) {
-			map2.put(topping[now_index], map2.getOrDefault(topping[now_index],0)+1);
-			map1.put(topping[now_index], map1.get(topping[now_index])-1);
-			if(map1.get(topping[now_index])==0) map1.remove(topping[now_index]);
-			if(map2.size()==map1.size()) {
-				answer++;
-			}
-			now_index ++;
-		}
+        int answer = 0;
+        HashMap<Integer,Integer> map1 = new HashMap<>();
+        HashMap<Integer,Integer> map2 = new HashMap<>();
+        for(int i=0;i<topping.length;i++){
+            map2.put(topping[i],map2.getOrDefault(topping[i],0)+1);
+        }
+        for(int i=0;i<topping.length;i++){
+            if(map1.size()==map2.size()) answer++;
+            map1.put(topping[i],map1.getOrDefault(topping[i],0)+1);
+            map2.put(topping[i],map2.get(topping[i])-1);
+            if(map2.get(topping[i])==0) map2.remove(topping[i]);
+            
+        }
+        
         return answer;
     }
 }
