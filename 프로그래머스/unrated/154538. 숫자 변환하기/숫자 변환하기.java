9ecaaed964de79af63db1ef0111da0d91a81bc81
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-06 15:14:34 +0900

    [unrated] Title: 숫자 변환하기, Time: 1.40 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,51 +1,32 @@
 import java.util.*;
 class Solution {
-	static boolean [] visited = new boolean[1000001];
-	static int answer = -1;
     public int solution(int x, int y, int n) {
-        if(x==y)return 0;
-		BFS(x,y,n);
+        int answer =BFS(x,y,n);
         return answer;
     }
-	private static void BFS(int x, int target, int n) {
-		Queue<T020> queue = new LinkedList<>();
-		queue.add(new T020(x, 0));
-		visited[x] = true;
-		while(!queue.isEmpty()) {
-			T020 test = queue.poll();
-			int a = test.su;
-			int b = test.depth;
-			if(test.su==target) {
-				answer = b;
-				return;
-			}
-			if(a<=1000000-n) {
-				if(!visited[a+n]&&a+n<=target) {
-					queue.add(new T020(a+n, b+1));
-					visited[a+n] = true;
-				}
-			}
-			if(a<=1000000/2) {
-				if(!visited[a*2]&&a*2<=target) {
-					queue.add(new T020(a*2, b+1));
-					visited[a*2] = true;
-				}
-			}
-			if(a<=1000000/3) {
-				if(!visited[a*3]&&a*3<=target) {
-					queue.add(new T020(a*3, b+1));
-					visited[a*3] = true;
-				}
-			}
-		}
-	}
-}
-class T020{
-	int su;
-	int depth;
-	
-	public T020(int su, int depth) {
-		this.su = su;
-		this.depth = depth;
-	}
+    private static int BFS(int x, int y, int n){
+        boolean [] visit = new boolean [1000001];
+        Queue<int []> queue = new LinkedList<>();
+        queue.add(new int []{x,0});
+        while(!queue.isEmpty()){
+            int [] A = queue.poll();
+            int su = A[0];
+            int depth = A[1];
+            
+            if(su ==y){return depth;}
+            if(su*2<=y&&!visit[su*2]){
+                queue.add(new int[]{su*2,depth+1});visit[su*2] = true;
+            }
+            if(su*3<=y&&!visit[su*3]){
+                queue.add(new int[]{su*3,depth+1});
+                visit[su*3] = true;
+            }
+            if(su+n<=y&&!visit[su+n]){
+                queue.add(new int[]{su+n,depth+1}); 
+                visit[su+n] = true;
+            }
+        }
+        return -1;
+        
+    }
 }
