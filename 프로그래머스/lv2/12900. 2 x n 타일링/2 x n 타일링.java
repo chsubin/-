62192b3daedf61069c273113f59e82d08faf88f9
Choi Subin <109835921+chsubin@users.noreply.github.com>
2023-06-06 17:29:30 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.99 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int n) {
-		int a = 1;
-		int b = 2;
-		int c = 0;
-		
-		for(int i=3;i<=n;i++) {
-			c = (a + b)%1000000007;
-			int temp;
-			 a = b ;b=c ;
-		}
+        int answer = 0;
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        for(int i=3;i<=n;i++){
+            c = (a+b)%1000000007;
+            a = b;
+            b = c;
+        }
         return c;
     }
 }
